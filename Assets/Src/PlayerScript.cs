b79eb3cb8448a24e0b2857c21b03560c5828a0ef
falufriSebastianLiljedal <79262917+falufriSebastianLiljedal@users.noreply.github.com>
2026-02-03 14:20:47 +0100

    Lektion 2

--- Assets/Src/PlayerScript.cs
+++ Assets/Src/PlayerScript.cs
@@ -5,6 +5,9 @@ public class PlayerScirpt : MonoBehaviour
     public float jumpSpeed = 1.0f;
     private Vector3 moveDirection = Vector3.zero;
     public float velocity = 10.0f;
+    public Rigidbody rb;
+    public float speedMultiplier = 1.0f;
+    public Camera camera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,17 +17,33 @@ public class PlayerScirpt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("space"))
+        //Camera.main.transform.eulerAngles.y
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        //shift run
+        if(Input.GetKeyDown("left shift"))
         {
-            transform.Translate(Vector3.up * jumpSpeed);
+            speedMultiplier = 1.5f;
         }
+        if(Input.GetKeyUp("left shift"))
+        {
+            speedMultiplier = 1.0f;
+        }
+
+        //jump
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+
+        //wasd movement
         if (Input.GetKey("d"))
         {
             moveDirection = Vector3.right;
         }
         else if (Input.GetKey("a"))
         {
-            moveDirection = Vector3.left;
+            moveDirection = Vector3.left ;
         }
         else if (Input.GetKey("w"))
         {
@@ -34,10 +53,18 @@ public class PlayerScirpt : MonoBehaviour
         {
             moveDirection = Vector3.back;
         }
-        else {             moveDirection = Vector3.zero;
+        else {             
+            moveDirection = Vector3.zero;
         }
 
-        transform.Translate(moveDirection * velocity * Time.deltaTime);
+        //rotation
+
+        if (0.0f<Input.mousePositionDelta.x) 
+        { 
+        
+        }
+
+        transform.Translate(moveDirection * velocity * speedMultiplier * Time.deltaTime);
     }
 
 
